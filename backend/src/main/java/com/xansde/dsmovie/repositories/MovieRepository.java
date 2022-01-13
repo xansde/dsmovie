4b0a5832716c40df319acf8ed5178a9d147fabcc
Xansde <xansde@gmail.com>
2022-01-13 00:35:29 -0300

    Find movies

---
+++ backend/src/main/java/com/xansde/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.xansde.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.xansde.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+	
+	
+}
